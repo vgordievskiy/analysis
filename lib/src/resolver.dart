c416edc5b3070cc7bca4eece612a3c2879422875
Matan Lurey <matan@lurey.org>
2015-06-06 15:16:33 -0700

    Replace existing implementation with a sharper one.

--- lib/src/resolver.dart
+++ lib/src/resolver.dart
@@ -4,25 +4,29 @@ import 'dart:io';
 
 import 'package:analyzer/src/generated/java_io.dart';
 import 'package:analyzer/src/generated/source_io.dart';
+import 'package:analyzer/src/generated/sdk.dart';
 
 /// A source code file resolver.
-class SourceResolver implements Function {
+class SourceResolver {
   static final _defaultPackageRoots = [Platform.packageRoot];
-  static const _filePrefix = 'file:/';
-  static const _packagePrefix = 'package:';
 
-  // TODO: Make private.
-  final PackageUriResolver packageUriResolver;
+  final PackageUriResolver _packageUriResolver;
 
-  /// Create a new default source resolver.
-  factory SourceResolver() {
-    return new SourceResolver.fromPackageRoots(_defaultPackageRoots);
+  /// Creates and returns a URI resolver from [packageRoots].
+  static PackageUriResolver _createResolver([Iterable<String> packageRoots]) {
+    final packageRootResolvers = packageRoots.map((packageRoot) {
+      return new JavaFile.fromUri(new Uri.file(packageRoot));
+    }).toList(growable: false);
+    return new PackageUriResolver(packageRootResolvers);
   }
 
-  /// Create a source resolver using the supplied as package roots.
+  /// Creates a default resolver, only using the platform package root.
+  factory SourceResolver() => new SourceResolver.fromPackageRoots(const []);
+
+  /// Creates a resolver form [packageRoots].
   factory SourceResolver.fromPackageRoots(
-      Iterable<String> packageRoots, [
-      bool includeDefaultPackageRoot = true]) {
+      Iterable<String> packageRoots, {
+      bool includeDefaultPackageRoot: true}) {
     final concatPackageRoots = packageRoots.toList();
     if (includeDefaultPackageRoot) {
       concatPackageRoots.addAll(_defaultPackageRoots);
@@ -30,31 +34,45 @@ class SourceResolver implements Function {
     return new SourceResolver._(_createResolver(concatPackageRoots));
   }
 
-  SourceResolver._(this.packageUriResolver);
+  SourceResolver._(this._packageUriResolver);
 
-  /// Resolve and returns [path]. For example:
-  ///     // Returns (for example) /home/ubuntu/user/libs/analysis/analysis.dart
-  ///     resolver('package:analysis/analysis.dart')
-  String call(String path) {
-    // Translate path to an absolute path.
-    if (path.startsWith(_packagePrefix)) {
-      var uri = Uri.parse(path);
-      path = packageUriResolver.resolveAbsolute(uri).toString();
-      final filePathIndex = path.indexOf(_filePrefix);
-      if (filePathIndex != -1) {
-        path = path.substring(filePathIndex + _filePrefix.length - 1);
-      }
-    } else {
-      throw new ArgumentError.value(path, 'path', 'Not in "package:" format.');
+  /// Return an instance of [SourceFactory] that uses the current package
+  /// resolution logic and one for [dartSdk].
+  SourceFactory createSourceFactory({
+      Iterable<UriResolver> addUriResolvers: const [],
+      DartSdk dartSdk,
+      bool useFileResolver: true}) {
+    final resolvers = <UriResolver> [];
+    if (dartSdk != null) {
+      resolvers.add(new DartUriResolver(dartSdk));
     }
-    return new JavaFile(path).getAbsolutePath();
+    if (useFileResolver) {
+      resolvers.add(new FileUriResolver());
+    }
+    resolvers.add(_packageUriResolver);
+    resolvers.addAll(addUriResolvers);
+    return new SourceFactory(resolvers);
   }
 
-  /// Creates and returns a URI resolver.
-  static PackageUriResolver _createResolver([Iterable<String> packageRoots]) {
-    final packageRootResolvers = packageRoots.map((packageRoot) {
-      return new JavaFile.fromUri(new Uri.file(packageRoot));
-    }).toList(growable: false);
-    return new PackageUriResolver(packageRootResolvers);
+  /// Given a [uri], returns a fully qualified file path.
+  ///
+  /// Example:
+  ///     resolve(new Uri('package:a/b/c.dart')) // ==> a/b/c.dart
+  String find(Uri uri) {
+    if (uri == null) {
+      throw new ArgumentError.notNull();
+    }
+    // Translate uri to an absolute path.
+    final source = _packageUriResolver.resolveAbsolute(uri);
+    return source.toString();
+  }
+
+  /// Given an absolute file [path], returns the package [Uri].
+  ///
+  /// Example:
+  ///     resolve('a/b/c.dart') // ==> package:a/b/c.dart
+  Uri resolve(String path) {
+    final source = new FileBasedSource.con1(new JavaFile(path));
+    return _packageUriResolver.restoreAbsolute(source);
   }
 }
---
+++ lib/testing/library/a.dart
@@ -0,0 +1,7 @@
+library lib_a;
+
+import 'b.dart';
+import 'package:analysis/testing/library/c.dart';
+export 'package:analysis/testing/library/d.dart';
+
+part 'a_part.dart';
